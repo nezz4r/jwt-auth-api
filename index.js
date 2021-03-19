891679340bc1a448ddf2e6fcf3d294374a07ab9d
Raul Queiroz <raul.queiroz@rga.com>
2021-03-19 20:44:11 -0300

    inital commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const authRoute = require('./routes/auth');
+
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => console.log('Server running'));
